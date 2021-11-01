afccf93e296324b4d36464061ec6b2d53755cfeb
samorenoq <samorenoq@eafit.edu.co>
2021-10-31 22:24:35 -0500

    Setup inicial para jmeter

---
+++ jmeter/setup.sh
@@ -0,0 +1,6 @@
+sudo apt install default-jre
+sudo apt install openjdk-11-jre-headless
+sudo apt install openjdk-9-jre-headless
+wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.1.tgz
+tar xzf apache-jmeter-5.4.1.tgz
+sudo rm apache-jmeter-5.4.1.tgz
